07be55a7f08913f30e55e6b7f0d7a9946e6944a0
Marsh1173 <natehroylance@gmail.com>
2024-05-05 22:16:00 -0600

    More imports updated

--- src/model/game/objects/model/ServerGameObject.ts
+++ src/model/game/objects/model/ServerGameObject.ts
@@ -1,5 +1,5 @@
-import { Platform } from "../platform/Platform";
-import { Portal } from "../portal/Portal";
-import { ServerShapelet } from "../shapelet/server/ServerShapelet";
+import { Platform } from "../objects/platform/Platform";
+import { Portal } from "../objects/portal/Portal";
+import { ServerShapelet } from "../objects/shapelet/server/ServerShapelet";
 
 export type ServerGameObjectType = ServerShapelet | Platform | Portal;
